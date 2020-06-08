d7b58b368521d15cfbd78cdcfbf2ecc196843f6c
BARS\d.pandyukhin <pandyukhin@yandex.ru>
2020-06-08 18:55:11 +0300

    page47

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using System;
 
 namespace PartyInvites.Controllers
 {
@@ -6,6 +8,8 @@ namespace PartyInvites.Controllers
     {
         public ViewResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
         }
     }
